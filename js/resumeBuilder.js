f2b677913bf1dd02d0993e1ceff54819cd19da95
Sergio <tabarinisergio@gmail.com>
2017-06-27 17:55:52 -0600

    First commit

---
+++ js/resumeBuilder.js
@@ -0,0 +1,11 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+ $(document).ready(function() {
+ 	$('#main').append('Mario');
+ 	var light = 299792458;
+ 	var meter = 100;
+ 	var second = 1.0/1000000000;
+ 	var result = (light * meter) / second;
+ 	$('#main').append(result);
+ });
