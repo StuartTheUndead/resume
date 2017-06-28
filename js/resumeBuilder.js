e678573c27b315b1d4ae65ebe563fed925d97449
Sergio <tabarinisergio@gmail.com>
2017-06-28 07:39:54 -0600

    Changes to resume js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,10 +2,23 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
  $(document).ready(function() {
+ 	var name = "Mario Tabarini";
+ 	var role = "Web Developer";
+ 	var formattedName = HTMLheaderName.replace("%data%", name);
+ 	var formattedRole = HTMLheaderRole.replace("%data%", role);
+	$("#head").prepend(formattedRole);
+	$("#head").prepend(formattedName);
+ 	/*
  	$('#main').append('Mario');
  	var light = 299792458;
  	var meter = 100;
  	var second = 1.0/1000000000;
  	var result = (light * meter) / second;
  	$('#main').append(result);
+ 	*/
  });
+/*
+var awesomeThoughts = "I am Stuardo and I am awesome";
+var funThoughts = awesomeThoughts.replace("awesome", "fun");
+console.log(awesomeThoughts);
+$('#main').append(funThoughts); */
