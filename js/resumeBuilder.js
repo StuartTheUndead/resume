ae366f1254a7b79df72fd3b9ee0615a3774844c1
Sergio <tabarinisergio@gmail.com>
2017-06-30 10:51:55 -0600

    MODIFIED: - Append

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,22 +1,25 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $(document).ready(function() {
- 	var name = "Mario Tabarini";
- 	var role = "Web Developer";
- 	var formattedName = HTMLheaderName.replace("%data%", name);
- 	var formattedRole = HTMLheaderRole.replace("%data%", role);
-	$("#head").prepend(formattedRole);
-	$("#head").prepend(formattedName);
- 	/*
+var name = "Mario Tabarini";
+var role = "Web Developer";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
+/*$(document).ready(function() {
+ 	
  	$('#main').append('Mario');
  	var light = 299792458;
  	var meter = 100;
  	var second = 1.0/1000000000;
  	var result = (light * meter) / second;
  	$('#main').append(result);
- 	*/
+ 	
  });
+*/
+
 /*
 var awesomeThoughts = "I am Stuardo and I am awesome";
 var funThoughts = awesomeThoughts.replace("awesome", "fun");
